ec8624d77b2b8507bd7110ce907f2876bada7109
Dreamboy-26 <heyimpankaj2616@gmail.com>
2022-04-11 13:18:45 +0530

    done

--- src/components/CartButton.jsx
+++ src/components/CartButton.jsx
@@ -1,14 +1,37 @@
-
+import { useState } from "react";
 // keep the add to cart component here
 const CartButton = () => {
     //manage state of the count 
+    let [count,setCount]=useState(1)
+
+    const handleInc=()=>{
+      setCount(count+1)
+    }
+    const handleDec=()=>{
+      
+      setCount(count-1)
+    }
+
+    const handleClick=()=>{
+  
+    }
   return <>
   {/* add to cart button */}
   {/* count with - and  + button */}
+<<<<<<< HEAD
+  
+  <button className="cart" onClick={handleClick}>Add To Cart  +</button>
+
+  <div className="countButton">
+      <button onClick={handleDec}>-</button>
+      <p className="count-item">{count}</p>
+      <button onClick={handleInc} >+</button>
+=======
   <div>
       <button>-</button>
-      <p className="count-item">{count}</p>
+      {/* <p className="count-item">{count}</p> */}
       <button>+</button>
+>>>>>>> fb1183de3dbf16e6bb11ba138f244c2e736a7ce7
   </div>
   </>;
 };
--- src/components/Grocerydetails.jsx
+++ src/components/Grocerydetails.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import data from '../data.json'
+import CartButton from "./CartButton"
 
 
 const GroceryDetails = ()=>{
@@ -10,6 +11,21 @@ console.log(data)
         <h1>Groceries</h1>
         <div className="container">
             {/* map through the data and display the cards */}
+           {data.map((item)=>{
+
+             return  <div className="innerContainer">
+                 <h3>{item.discount}</h3>
+                   <img src={item.imgURL} alt="" />
+                   <p>{item.title}</p>
+                   <div className="price">
+                       <p>{item.sellingPrice}</p>
+                       <p>M.R.P :</p>
+                       <p className="mrp">{item.mrp}</p>
+                   </div>
+                  <CartButton></CartButton>
+               </div>
+               
+           })}
         </div>
         </>
     )
